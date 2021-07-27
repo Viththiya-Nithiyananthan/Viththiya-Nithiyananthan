7fa7f19960a473c7f9ac6051f800525ea8aa7841
Viththiya-Nithiyananthan <87899928+Viththiya-Nithiyananthan@users.noreply.github.com>
2021-07-27 19:17:53 +0530

    question 4.js

---
+++ question 4.js
@@ -0,0 +1,7 @@
+const length = 6;
+const height = 2;
+    function findcuboid(width){
+      let cuboid = length*width*height;
+      eturn cuboid
+    };
+document.getElementById("demo").innerHTML= "The cuboid value is : "+" " +findcuboid(4);
