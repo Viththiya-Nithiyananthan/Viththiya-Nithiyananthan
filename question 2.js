ebd4a41d8b3386c2fcd5c93a7002ca8f64f8981e
Viththiya-Nithiyananthan <87899928+Viththiya-Nithiyananthan@users.noreply.github.com>
2021-07-27 19:12:38 +0530

    question 2.js

---
+++ question 2.js
@@ -0,0 +1,28 @@
+<function addNum(a, b) {
+  return a + b;
+}
+{
+document.getElementById("addNum").innerHTML = addNum(10, 2);
+}
+
+function multiNum(a, b) {
+  return a * b;
+}
+{
+document.getElementById("multiNum").innerHTML = multiNum(10, 2);
+}
+
+
+function subtractNum(a, b) {
+  return a - b;
+}
+{
+document.getElementById("subtractNum").innerHTML = subtractNum(10, 2);
+}
+
+function divideNum(a, b) {
+  return a / b;
+}
+{
+document.getElementById("divideNum").innerHTML =divideNum(10, 2);
+}
